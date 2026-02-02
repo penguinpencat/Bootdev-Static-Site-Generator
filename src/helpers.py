b468dea87398c06069aa56662f7fbd70e05f8b22
penguinpencat <melanacaffullfreear@gmail.com>
2026-02-02 17:31:31 +0000

    11. Implemented the split_node_images and split_node_links Functions with Tests

--- src/helpers.py
+++ src/helpers.py
@@ -1,4 +1,5 @@
 from textnode import TextNode, TextType
+from regex_help import *
 
 def split_nodes_delimiter(old_nodes: list[TextNode], delimiter: str, text_type: TextType):
     match delimiter:
@@ -15,9 +16,52 @@ def split_nodes_delimiter(old_nodes: list[TextNode], delimiter: str, text_type: 
         else:
             splitteded_strings = old_node.text.split(delimiter)
             if len(splitteded_strings) != 3:
-                raise Exception("You haven't (ENDED THE DELIMITER)")
+                raise Exception("You haven't (ENDEDed THE DELIMITER)")
             new_list.append(TextNode(splitteded_strings[0], TextType.TEXT))
             new_list.append(TextNode(splitteded_strings[1], delimiter_type))
             new_list.append(TextNode(splitteded_strings[2], TextType.TEXT))
 
     return new_list
+
+
+def split_nodes_image(old_nodes: list[TextNode]):
+    new_list = []
+    for old_node in old_nodes:
+        text_to_split: str = old_node.text
+        matches = extract_markdown_images(old_node.text)
+        for match in matches:
+            delimeter_string = f"![{match[0]}]({match[1]})"
+            sections = text_to_split.split(delimeter_string, 1)
+            new_list.append(TextNode(sections[0], TextType.TEXT))
+            new_list.append(TextNode(match[0], TextType.IMAGE, match[1]))
+            text_to_split = sections[1]
+        if text_to_split != "":
+            new_list.append(TextNode(text_to_split, TextType.TEXT))
+    return new_list
+
+
+def split_nodes_link(old_nodes: list[TextNode]):
+    new_list = []
+    for old_node in old_nodes:
+        text_to_split: str = old_node.text
+        matches = extract_markdown_links(old_node.text)
+        for match in matches:
+            delimeter_string = f"[{match[0]}]({match[1]})"
+            sections = text_to_split.split(delimeter_string, 1)
+            new_list.append(TextNode(sections[0], TextType.TEXT))
+            new_list.append(TextNode(match[0], TextType.LINK, match[1]))
+            text_to_split = sections[1]
+        if text_to_split != "":
+            new_list.append(TextNode(text_to_split, TextType.TEXT))
+    return new_list
+            
+            
+
+
+node = TextNode(
+    "This is text with a link [to boot dev](https://www.boot.dev) and [to youtube](https://www.youtube.com/@bootdotdev)",
+    TextType.TEXT,
+)
+print(split_nodes_link([node]))
+
+
--- src/test_helpers.py
+++ src/test_helpers.py
@@ -27,3 +27,59 @@ class TestHelper(unittest.TestCase):
                 TextNode(" word", TextType.TEXT)
             ]
         )
+
+    def test_split_images(self):
+        node = TextNode(
+            "This is text with an ![image](https://i.imgur.com/zjjcJKZ.png) and another ![second image](https://i.imgur.com/3elNhQu.png)",
+            TextType.TEXT,
+        )
+        new_nodes = split_nodes_image([node])
+        self.assertListEqual(
+            [
+                TextNode("This is text with an ", TextType.TEXT),
+                TextNode("image", TextType.IMAGE, "https://i.imgur.com/zjjcJKZ.png"),
+                TextNode(" and another ", TextType.TEXT),
+                TextNode(
+                    "second image", TextType.IMAGE, "https://i.imgur.com/3elNhQu.png"
+                ),
+            ],
+            new_nodes,
+        )
+
+    def test_split_links(self):
+        node = TextNode(
+            "This is text with an [link](https://www.daddy-is-the-best.com) and another [link, who wouldda guessed it](https://https://www.youtube.com/@bootdotdev)",
+            TextType.TEXT,
+        )
+        new_nodes = split_nodes_link([node])
+        self.assertListEqual(
+            [
+                TextNode("This is text with an ", TextType.TEXT),
+                TextNode("link", TextType.LINK, "https://www.daddy-is-the-best.com"),
+                TextNode(" and another ", TextType.TEXT),
+                TextNode(
+                    "link, who wouldda guessed it", TextType.LINK, "https://https://www.youtube.com/@bootdotdev"
+                ),
+            ],
+            new_nodes,
+        )
+
+    def test_split_links_with_extra(self):
+        node = TextNode(
+            "This is text with an [link](https://www.daddy-is-the-best.com) and another [link, who wouldda guessed it](https://https://www.youtube.com/@bootdotdev) to boot.dev",
+            TextType.TEXT,
+        )
+        new_nodes = split_nodes_link([node])
+        self.assertListEqual(
+            [
+                TextNode("This is text with an ", TextType.TEXT),
+                TextNode("link", TextType.LINK, "https://www.daddy-is-the-best.com"),
+                TextNode(" and another ", TextType.TEXT),
+                TextNode(
+                    "link, who wouldda guessed it", TextType.LINK, "https://https://www.youtube.com/@bootdotdev"
+                ),
+                TextNode(" to boot.dev", TextType.TEXT)
+            ],
+            new_nodes,
+        )
+
