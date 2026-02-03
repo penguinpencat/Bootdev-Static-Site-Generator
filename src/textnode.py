2cc17849200a5130408fac889d98e96e66ca3c6c
penguinpencat <melanacaffullfreear@gmail.com>
2026-02-03 21:22:35 +0000

    13. Type hint TextNode

--- src/textnode.py
+++ src/textnode.py
@@ -11,7 +11,7 @@ class TextType(Enum):
 
 class TextNode():
 
-    def __init__(self, text, text_type, url=None):
+    def __init__(self, text: str, text_type: TextType, url: str=None):
         self.text = text
         self.text_type = text_type
         self.url = url
